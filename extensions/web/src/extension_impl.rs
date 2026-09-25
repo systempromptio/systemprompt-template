e735576b04cbb50670da31dd8e3936cafb7f49ce
Edward <ed@systemprompt.io>
2026-09-25 15:21:31 +0200

    test: refresh extension graph test lockfile

--- extensions/web/src/extension_impl.rs
+++ extensions/web/src/extension_impl.rs
@@ -102,12 +102,7 @@ impl Extension for WebExtension {
     }
 
     fn dependencies(&self) -> Vec<&'static str> {
-        vec![
-            "content",
-            "users",
-            "authz",
-            "managed_resources",
-        ]
+        vec!["content", "users", "authz", "managed_resources"]
     }
 
     fn cross_extension_tables(&self) -> Vec<&'static str> {
